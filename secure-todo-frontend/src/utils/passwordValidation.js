0cf695701422309e58ff9da22865bc59275a6b4c
BasamDawii <91468338+BasamDawii@users.noreply.github.com>
2025-05-22 14:46:20 +0200

    Password-Requairements-CheckMarks

--- secure-todo-frontend/src/utils/passwordValidation.js
+++ secure-todo-frontend/src/utils/passwordValidation.js
@@ -1,39 +1,19 @@
-export const validatePassword = (password) => {
-  const errors = [];
-
-  if (password.length < 8) {
-    errors.push("Password must be at least 8 characters long");
-  }
-  if (!/[A-Z]/.test(password)) {
-    errors.push("Password must contain at least one uppercase letter");
-  }
-  if (!/[a-z]/.test(password)) {
-    errors.push("Password must contain at least one lowercase letter");
-  }
-  if (!/[0-9]/.test(password)) {
-    errors.push("Password must contain at least one number");
-  }
-  if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
-    errors.push("Password must contain at least one special character");
-  }
-  if (/(.)\1{2,}/.test(password)) {
-    errors.push("Password contains too many repeating characters");
-  }
-
-  const commonPasswords = [
-    "password", "123456", "12345678", "qwerty", "abc123",
-    "monkey", "letmein", "dragon", "111111", "baseball",
-    "iloveyou", "trustno1", "sunshine", "master", "welcome",
-    "shadow", "ashley", "football", "jesus", "michael",
-    "ninja", "mustang", "password1", "123456789", "password123"
-  ];
-
-  if (commonPasswords.includes(password.toLowerCase())) {
-    errors.push("Password is too common");
-  }
-
-  return {
-    isValid: errors.length === 0,
-    errors
+export const checkPasswordRules = (password) => {
+  const rules = {
+    minLength: password.length >= 8,
+    hasUppercase: /[A-Z]/.test(password),
+    hasLowercase: /[a-z]/.test(password),
+    hasNumber: /[0-9]/.test(password),
+    hasSpecialChar: /[!@#$%^&*(),.?":{}|<>]/.test(password),
+    noRepeats: !(/(.)\1{2,}/.test(password)),
+    notCommon: ![
+      "password", "123456", "12345678", "qwerty", "abc123",
+      "monkey", "letmein", "dragon", "111111", "baseball",
+      "iloveyou", "trustno1", "sunshine", "master", "welcome",
+      "shadow", "ashley", "football", "jesus", "michael",
+      "ninja", "mustang", "password1", "123456789", "password123"
+    ].includes(password.toLowerCase())
   };
-}; 
+
+  return rules;
+};
